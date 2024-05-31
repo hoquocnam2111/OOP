3a5a1b38c0701f9616d46cb1e786b8513797eccd
hoquocnam2111 <hoquocnam92@gmail.com>
2024-05-31 23:39:54 +0700

    add friend

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -7,13 +7,17 @@ class SinhVien{
 		static int dem ;
 
 	public:
-		// SinhVien();
-		// void nhap();
- 		// void in();
- 		// float getGpa();
+		SinhVien();
+		void nhap();
+ 		void in();
+ 		float getGpa();
 		void tangdem();
 		int getdem();
+		friend void inthongtin(SinhVien);
 };
+void inthongtin(SinhVien a){
+	cout<<a.id<<" "<<a.ten;
+}
 int SinhVien::dem=0;
 
 void SinhVien::tangdem(){
@@ -22,20 +26,22 @@ void SinhVien::tangdem(){
 int SinhVien::getdem(){
 	return dem;
 }
-// SinhVien::SinhVien(){
-// 	id=ten=ns="";
-// 	gpa=0;
-// }
-// void SinhVien::nhap(){
-// 	cout<<"Nhap ID : ";cin>>id;
-// 	cin.ignore();
-// 	cout<<"Nhap Ten : ";getline(cin,ten);
-// 	cout<<"Nhap Ns: ";cin>>ns;
-// 	cout<<"Nhap Diem: ";cin>>gpa;
-// }
-// void SinhVien::in(){
-// 	cout<<id<<" "<<ten<<" "<<ns<<" "<<gpa<<" "<<endl;
-// }
+SinhVien::SinhVien(){
+	ten=ns="";
+	gpa=0;
+}
+void SinhVien::nhap(){
+	++dem;
+	cin.ignore();
+	this->id="SV"+string(3-to_string(dem).length(),'0')+to_string(dem);
+	cout<<"Nhap Ten : ";getline(cin,ten);
+	cout<<"Nhap Ns: ";cin>>ns;
+	cout<<"Nhap Diem: ";cin>>gpa;
+
+}
+void SinhVien::in(){
+	cout<<id<<" "<<ten<<" "<<ns<<" "<<gpa<<" "<<endl;
+}
 // float SinhVien::getGpa(){
 // 	return this->gpa;
 // }
@@ -43,11 +49,17 @@ int SinhVien::getdem(){
 // 	return a.getGpa()>b.getGpa();
 // }
 int main(){
-	 
-	SinhVien x;
-	x.tangdem();
-	x.tangdem();
-	SinhVien y;
-	cout<<y.getdem()<<endl;
+	cout<<"Nhap so luong Sinh Vien : ";
+	 int n;cin>>n;
+	 SinhVien a[100];
+	 for(int i=0;i<n;i++){
+		a[i].nhap();
+	 }
+	 for(int i=0;i<n;i++){
+		a[i].in();
+		inthongtin(a[i]);
+	 }
+	
+	
 	return 0;
 }
